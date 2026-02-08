94645581ed5cd5b8d31200a601b089d60150b62e
Krishna <kd259457@gmail.com>
2026-02-09 01:13:39 +0530

    Update mustjoin.py

--- DAXXMUSIC/plugins/bot/mustjoin.py
+++ DAXXMUSIC/plugins/bot/mustjoin.py
@@ -6,7 +6,7 @@ from DAXXMUSIC import app
 
 #--------------------------
 
-MUST_JOIN = "CARD3DBOTx"
+MUST_JOIN = "SECRECT_BOT_UPDATES"
 #------------------------
 @app.on_message(filters.incoming & filters.private, group=-1)
 async def must_join_channel(app: Client, msg: Message):
